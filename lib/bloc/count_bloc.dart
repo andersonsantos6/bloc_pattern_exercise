6da4f68d9004333c2d07e29211b8f422fa5c444a
anderson santos <andersonsantos.programer@gmail.com>
2022-10-25 10:43:33 -0300

    refatoracao

--- lib/bloc/count_bloc.dart
+++ lib/bloc/count_bloc.dart
@@ -2,12 +2,34 @@ import 'package:bloc/bloc.dart';
 
 abstract class CounterEvent {}
 
+abstract class CounterState {
+  final int count;
+  CounterState({required this.count});
+}
+
+class InitialCountState extends CounterState {
+  InitialCountState() : super(count: 0);
+}
+
+class IncrementState extends CounterState {
+  IncrementState({required int count}) : super(count: count);
+}
 
 class Increment extends CounterEvent {}
 
-class CounterBloc extends Bloc<CounterEvent, int> {
-  CounterBloc() : super(0) {
-    on((event, emit) => emit(state + 1));
+class Decrement extends CounterEvent {}
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc() : super(InitialCountState()) {
+    on((event, emit) {
+      if (event is Increment) {
+        emit(IncrementState(count: state.count + 1));
+        return;
+      }
+      if (event is Decrement) {
+        emit(IncrementState(count: state.count - 1));
+        return;
+      }
+    });
   }
-  
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,8 @@ import 'package:bloc_pattern_exercise/bloc/count_bloc.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -18,28 +17,36 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Bloc Pattern'),
         ),
-        body: Center(
-          child: StreamBuilder(
-            stream: bloc.stream,
-            builder: (context, snapshot) {
-              if (bloc.state == 0) {
-                return const Text('valor zerado...');
-              } else {
-                return Text(bloc.state.toString());
-                
-              }
-            },
-          ),
-          
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            StreamBuilder(
+              stream: bloc.stream,
+              builder: (context, snapshot) {
+                if (bloc.state == 0) {
+                  return const Text('valor zerado...');
+                } else {
+                  return Text(bloc.state.count.toString());
+                }
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                    onPressed: () => bloc.add(Decrement()),
+                    child: const Text('Decrement')),
+                TextButton(
+                    onPressed: () => bloc.add(Increment()),
+                    child: const Text('Increment')),
+              ],
+            )
+          ],
         ),
-        floatingActionButton: FloatingActionButton(
-            onPressed: () => bloc.add(Increment()),
-            child: const Icon(Icons.add)),
       ),
     );
-  
   }
-  
-  
 }
-
